77b7d302ec449519c77401c95fd9cc02324f867f
Bilal Rashid <bilaalrashid5566@gmail.com>
2024-01-29 22:13:13 +0500

    some changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import Link from 'next/link';
 import { Flex, Button } from 'antd'
+
+
+
 const Navbar: React.FC = () => {
     return (
         <>
@@ -8,13 +11,13 @@ const Navbar: React.FC = () => {
                 <h1 className='font-bold text-2xl'>Logo</h1>
             </Link>
             <Flex align='center' gap={20}>
-                <Button type='default' size='large'>
-                    Home
-                </Button>
-                <Button type='default' size='large'>
+                <Button type='text' size='middle'>
                     About
                 </Button>
-                <Button type='default' size='large'>
+                <Button type='text' size='middle'>
+                    About
+                </Button>
+                <Button type='text' size='middle'>
                     Category
                 </Button>
             </Flex>
